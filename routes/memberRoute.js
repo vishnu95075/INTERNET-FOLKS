17f67e26bb46378c57df2f30b77b97f14454ca4d
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2024-01-01 13:40:28 +0530

    update authorizeRoles

--- routes/memberRoute.js
+++ routes/memberRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { addMember, removeMember } = require('../controllers/memberController');
+const { authorizeRoles, isAuthenticatedUser } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route("/member").post(addMember);
-router.route("/member/:id").delete(removeMember);
+router.route("/member/:id").delete(isAuthenticatedUser,authorizeRoles("Community Admin","Community Moderator"),removeMember);
 
 module.exports = router
