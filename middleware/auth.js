81849c5376ce0a89a3c48bea36d28e1535ef7dd9
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2024-01-01 13:35:58 +0530

    update authorizeRoles

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,35 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncError = require("./catchAsyncError")
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel")
-exports.isAuthenticatedUser = catchAsyncError(async(req,res,next)=>{
-    const{ token} =req.cookies;
+const User = require("../models/userModel");
+const Role = require("../models/roleModel");
+const Member = require("../models/memberModel");
+
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+    const { token } = req.cookies;
     // console.log(token);
-    if(!token){
-        return next(new ErrorHandler("Error - Not Signed In"),401);
+    if (!token) {
+        return next(new ErrorHandler("Error - Not Signed In"), 401);
     }
 
-    const decodeData = jwt.verify(token,process.env.JWT_SECRET_KEY);
+    const decodeData = jwt.verify(token, process.env.JWT_SECRET_KEY);
     // console.log("decodeData  => ",decodeData)
-    req.user=await User.findById(decodeData.id);
+    req.user = await User.findById(decodeData.id);
     // console.log("req.user", req.user);
     next();
 });
 
-exports.authorizeRoles = (...roles)=>{
-    return (req,res,next)=>{
-        if(!roles.includes(req.user.role)){
-        return next(
-            new ErrorHandler(`Role : ${req.user.role} is not allow to access this resource `,403
-            ));
+exports.authorizeRoles = (...roles) => {
+    return async (req, res, next) => {
+        const member = await Member.findOne({ user: req.user.id }).populate("role", "name");
+        if (!member) {
+            return next(new ErrorHandler(`User not found.`, 404));
         }
+        const userRole = member.role.name;
+        if (!roles.includes(userRole)) {
+            return next(new ErrorHandler(`Role: ${userRole} is not allowed to access this resource.`, 403));
+        }
+
         next();
     }
 }
--- routes/communityRoute.js
+++ routes/communityRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { createCommunity, getAllCommunities, getAllCommunityMembers, getMyOwnedCommunity, getMyJoinCommunity } = require('../controllers/communityController');
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 const router = express.Router();
 
-router.route("/community").post(isAuthenticatedUser,createCommunity);
+router.route("/community").post(isAuthenticatedUser,authorizeRoles("Community Admin"),createCommunity);
 router.route("/community").get(getAllCommunities);
 router.route("/community/:id/members").get(isAuthenticatedUser,getAllCommunityMembers);
 router.route("/community/me/owner").get(isAuthenticatedUser,getMyOwnedCommunity);
